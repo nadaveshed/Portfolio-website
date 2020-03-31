9c6d18b8a42e3ceb386806c152878094e35e4985
נדב אשד <nadav3200@gmail.com>
2020-03-31 15:41:36 +0300

    Portfolio website

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function($){
+    var typed = new Typed('span.txt-rotate', {
+        strings: ["I'm a Web Developer", "I'm a Fullstack Developer", "I'm a Programmer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackspace: true,
+        loop: true
+    });
+})(jQuery);
